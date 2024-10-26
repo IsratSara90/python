8453a2d09eceb5d06567d37b50eb7a561b1e2c23
IsratSara90 <168336343+IsratSara90@users.noreply.github.com>
2024-10-26 19:21:09 +0600

    Add files via upload

---
+++ code.py
@@ -0,0 +1,27 @@
+#list
+mylist = ["EMU","Shikha","Soniya","Arat"]
+print(mylist)
+print(len(mylist))
+
+#tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+print(len(thistuple))
+print(type(thistuple))
+
+
+#set
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(set1)
+print(len(set1))
+print(type(set1))
+
+#dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
